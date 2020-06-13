93983cabc236620b2f55d9b878678de6a53e9bc3
Jerriesfly <a15821920957@gmail.com>
2020-06-13 20:35:03 +0800

    lab11

--- lab11/config.php
+++ lab11/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DBHOST', 'localhost');
-define('DBNAME', 'art');
-define('DBUSER', 'root');
-define('DBPASS', '123456');
-define('DBCONNSTRING','mysql:host=localhost;dbname=art');
+define('DBNAME', 'Login');
+define('DBUSER', 'Jerry');
+define('DBPASS', 'Zalone1314520');
+define('DBCONNSTRING','mysql:dbname=Login;charset=utf8mb4;');
 ?>
--- lab11/lab11-exercise1.php
+++ lab11/lab11-exercise1.php
@@ -1,11 +1,11 @@
 <html lang="en">
 <head>
 
-<!-- Latest compiled and minified Bootstrap Core CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <!-- Latest compiled and minified Bootstrap Core CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   <title>Exercise 11-1 | Using Cookies</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Exercise 11-1 | Using Cookies</title>
 </head>
 
 <body>
@@ -14,8 +14,9 @@
 
 
 <?php
-function getLoginForm(){
-   return "
+function getLoginForm()
+{
+    return "
 <form action='' method='post' role='form'>
 <div class ='form-group'>
   <label for='username'>Username</label>
@@ -26,26 +27,63 @@ function getLoginForm(){
   <input type='password' name='pword' class='form-control'/>
 </div>
 <input type='submit' value='Login' class='form-control' />
-
 </form>";
 }
-?>
- <div class="container theme-showcase" role="main">  
-      <div class="jumbotron">
-        <h1>
-<?php
-   require_once("config.php");
-   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     echo "Login attempted";
-   }
-   else{
-     echo "No Post detected";
-   } 
-?>
 
-</h1>
-      </div>
-<?php echo getLoginForm(); ?>
- </div>
+
+
+function validLogin()
+{
+    $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+    //very simple (and insecure) check of valid credentials.
+    $sql = "SELECT * FROM Credentials WHERE Username=:user and Password=:pass";
+
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':user', $_POST['username']);
+    $statement->bindValue(':pass', $_POST['pword']);
+    $statement->execute();
+    if ($statement->rowCount() > 0) {
+        return true;
+    }
+    return false;
+}
+
+?>
+<div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+        <h1>
+            <?php
+            require_once("config.php");
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (validLogin()) {
+                    // add 1 day to the current time for expiry time
+                    $_SESSION['Username']=$_POST['username'];
+                } else {
+                    echo "login unsuccessful";
+                }
+            }
+            if (isset($_SESSION['Username'])) {
+                echo "Welcome " . $_SESSION['Username'];
+
+            } else {
+                echo "No Post detected";
+            }
+            ?>
+
+        </h1>
+    </div>
+    <?php if (!isset($_SESSION['Username'])) {
+        echo getLoginForm();
+    } else {
+        echo "This is some content";
+    } ?>
+    <div>
+        <form action="logout.php" method="post" role="form">
+            <input type="submit" value="clear cookie">
+        </form>
+    </div>
+</div>
+
 </body>
 </html>
